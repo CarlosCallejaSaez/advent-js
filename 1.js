2894495c7e2697bbe615a04a13151396bc9411e3
CarlosCallejaSaez <carloscallejasez@gmail.com>
2023-12-10 20:16:49 +0100

    advent 1 fixed

--- 1.js
+++ 1.js
@@ -1,22 +1,15 @@
 function findFirstRepeated(gifts) {
-    const auxiliar=[];
-
-    for(let i=0; i<gifts.length; i++) {
-        if(auxiliar[gifts[i]]!==undefined){
-            return gifts[i];
-        }
-        else{
-            auxiliar[i]=gifts[i];
-        }
+    const aux=[];
+  
+    for(let i=0;i<gifts.length;i++){
+       
+       if(aux.includes(gifts[i])) return gifts[i]
+       else aux.push(gifts[i])
+  
+  
+  
     }
-        return -1
-
-
+  
+    return -1
   }
-  const giftIds = [2, 1, 3, 5, 3, 2]
-const firstRepeatedId = findFirstRepeated(giftIds)
-console.log(firstRepeatedId)
-
-const giftIds2 = [1, 2, 3, 4]
-const firstRepeatedId2 = findFirstRepeated(giftIds2)
-console.log(firstRepeatedId2)
+     
